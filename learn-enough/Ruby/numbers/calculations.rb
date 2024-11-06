2a460314f1895635e5119abed92b4b599f1b831d
Alokananda Y <plantsfortrees@gmail.com>
2024-11-06 12:38:31 +0000

    Python in a web browser

---
+++ learn-enough/Ruby/numbers/calculations.rb
@@ -0,0 +1,11 @@
+# Hours in a year
+hours_in_a_day = 24
+days_in_a_year = 365
+hours_in_a_year = hours_in_a_day * days_in_a_year
+puts(hours_in_a_year)
+print "\n\n"
+minutes_in_an_hour = 60
+minutes_in_a_decade = days_in_a_year * hours_in_a_day * minutes_in_an_hour
+puts minutes_in_a_decade
+print "\n\n"
+

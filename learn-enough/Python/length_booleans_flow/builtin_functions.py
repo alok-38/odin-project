5b0771a351412bed51cc882547b2ab5ffa222f14
Alokananda Y <plantsfortrees@gmail.com>
2024-11-07 00:29:28 +0000

    All about strings

---
+++ learn-enough/Python/length_booleans_flow/builtin_functions.py
@@ -0,0 +1,2 @@
+print(len("Hello, world!"))
+
---
+++ learn-enough/Ruby/strings/string_programs1.rb
@@ -0,0 +1,20 @@
+puts 'This is a single quited string'
+puts "This is a double quoted string"
+print "Printing using print!"
+print "\n\n"
+my_string = "This is my string"
+puts my_string
+# String Concatenation
+puts "Sammy" + " " + "shark"
+my_age = 40
+my_name = "Sammy"
+puts "I'm #{my_name} and I turned #{my_age}"
+print "\n\n"
+# Strings and integers
+my_name = "Sammy the Shark"
+my_number = 27
+puts "#{my_name} #{my_number}"
+print "\n\n"
+puts "#{my_name} #{my_number}".to_s
+print "\n\n"
+

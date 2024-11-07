b772fd7c4f98249180c70e2e933e5ff4d0f20e8b
Alokananda Y <plantsfortrees@gmail.com>
2024-11-07 00:45:27 +0000

    String methods in Ruby

--- learn-enough/Ruby/strings/string_programs1.rb
+++ learn-enough/Ruby/strings/string_programs1.rb
@@ -17,4 +17,50 @@ puts "#{my_name} #{my_number}"
 print "\n\n"
 puts "#{my_name} #{my_number}".to_s
 print "\n\n"
+# Quoting
+puts "Sammy says, \"I'm a happy shark!\""
+print "\n\n"
+puts %$Sammy says, "I'm a happy shark!"$
+print "\n\n"
+droplets = 5
+puts %$Sammy says, I have #{droplets} droplets.$
+print "\n\n"
+puts %Q{Sammy says, "I just created #{droplets + 1} droplets!"}
+print "\n\n"
+# Long strings and new lines
+output = "This is\na string\nwith newline"
+puts output
+print "\n\n"
+# You can also use any of the alternate string syntaxes to create multiline strings:
+output = %{This is a
+           longer string
+           with newlines
+}
+puts output
+print "\n\n"
+# Heredoc
+output = <<-END
+This is a
+longer string
+with newlines
+END
+puts output
+print "\n\n"
+# squiggly heredoc”
+output = <<~END
+This is a
+longer string
+with newlines
+and the code is intended
+but the output is not.
+END
+print "\n\n"
+# String Replication
+puts "Sammy" * 9
+print "\n\n"
+# ASCII art
+puts "=" * 15
+puts "| Hello World |"
+puts "=" * 15
+print "\n\n"
 

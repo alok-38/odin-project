54850e9c31f2a0c2d10c49bebc803d40276fa7d1
Alokananda Y <plantsfortrees@gmail.com>
2025-02-22 03:27:48 +0000

    VAT Calculation Tool

--- 100Devs/JavaScript/what_is_today/app.js
+++ 100Devs/JavaScript/what_is_today/app.js
@@ -1,9 +1,46 @@
 const inputEl = document.querySelector('input');
-const butonEl = document.querySelector('button');
+const buttonEl = document.querySelector('button');
 
 function check() {
-    const day = inputEl.value;
-    // Conditionals go here
+    const day = inputEl.value.toLowerCase().trim(); // Getting the day and standardizing the input
+    let message = '';
+
+    // Conditionals to check the day
+    switch (day) {
+        case 'monday':
+            message = 'Start of the week!';
+            inputEl.value = "";
+            break;
+        case 'tuesday':
+            message = 'Keep going!';
+            break;
+        case 'wednesday':
+            message = 'Halfway through!';
+            inputEl.value = "";
+            break;
+        case 'thursday':
+            message = 'Almost there!';
+            inputEl.value = "";
+            break;
+        case 'friday':
+            message = 'It\'s finally Friday!';
+            inputEl.value = "";
+            break;
+        case 'saturday':
+            message = 'Weekend vibes!';
+            inputEl.value = "";
+            break;
+        case 'sunday':
+            message = 'Relax and recharge!';
+            inputEl.value = "";
+            break;
+        default:
+            message = 'Please enter a valid day of the week!';
+            inputEl.value = "";
+            break;
+    }
+
+    alert(message); // Show a message based on the input
 }
 
-butonEl.addEventListener('click', check);
+buttonEl.addEventListener('click', check);

fbbc7168907d71bf0bf85e0c703f3ebd29a4043c
Alokananda Y <plantsfortrees@gmail.com>
2025-04-07 23:25:45 +0000

    Building & Deploy a TODO APP with JavaScript

--- store-picker/src/components/StorePicker.jsx
+++ store-picker/src/components/StorePicker.jsx
@@ -1,10 +1,6 @@
 function StorePicker(props) {
     return (
-        <header>
-            <h1>Please enter a store</h1>
-            <p>worried-fancy-data</p>
-            <button>VISIT STORE &#8594;</button>
-        </header>
+       <p>Hello</p>
     )
 }
 

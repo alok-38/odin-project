3cc241bdec512c0b04565b5ab0c774ead9dc6907
Alokananda Y <plantsfortrees@gmail.com>
2025-04-07 11:07:13 +0000

    Creating my first react component

---
+++ store-picker/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import StorePicker from './components/StorePicker'
+
+function App() {
+
+  return (
+    <main>
+      <StorePicker />
+    </main>
+  )
+}
+
+export default App
---
+++ store-picker/src/components/StorePicker.jsx
@@ -0,0 +1,12 @@
+function StorePicker(props) {
+    return (
+        <header>
+            <h1>Please enter a store</h1>
+            <p>worried-fancy-data</p>
+            <button>VISIT STORE &#8594;</button>
+        </header>
+    )
+}
+
+
+export default StorePicker
